588b746933cd306111aa4aa5559d42dddd2c6e74
Tara Z. Manicsic <tzmanics@gmail.com>
2022-11-03 11:08:02 -0400

    adds stuff for sitemaps and releases

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
+module.exports = {
+  // other webpack configuration
+  devtool: "source-map",
+  plugins: [
+    new SentryWebpackPlugin({
+      // sentry-cli configuration - can also be done directly through sentry-cli
+      // see https://docs.sentry.io/product/cli/configuration/ for details
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "netlify-uy",
+      project: "vue-test",
+      release: process.env.SENTRY_RELEASE,
+
+      // other SentryWebpackPlugin configuration
+      include: ".",
+      ignore: ["node_modules", "webpack.config.js"],
+    }),
+  ],
+};
